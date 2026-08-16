8781301141b1f96eaa44611d40aaf2007bac3cb4
shashankshekhar032005-art <shashankshekar032005@gmail.com>
2026-08-17 01:11:05 +0530

    Fix component import names

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 import Home from "./pages/home";
 import Login from "./pages/login";
