5f769b60afc3f70217c784104af3b63f80e71d1d
shashankshekhar032005-art <shashankshekar032005@gmail.com>
2026-08-17 01:20:02 +0530

    Fix ItemCard import

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import ItemCard from "../components/ItemCard";
+import ItemCard from "../components/itemcard";
 
 function Home() {
   const [search, setSearch] = useState("");
